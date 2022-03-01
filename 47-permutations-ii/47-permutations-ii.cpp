e9ea1f29756183bdc3c5fadc9a48de6ebde82391
Aditya Nath Jha <jhaaditya757@gmail.com>
2022-03-01 19:26:44 +0530

    Time: 3 ms (97.41%), Space: 8.2 MB (99.96%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& a) {
+        sort(a.begin(), a.end());
+        vector<vector<int>> res;
+        res.push_back(a);
+        while(next_permutation(a.begin(), a.end())) {
+            res.push_back(a);
+        }
+        return res;
+    }
+};
