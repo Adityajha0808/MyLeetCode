36bc1789c3f69e1d002026a235dc399b2f013dd4
Aditya Nath Jha <jhaaditya757@gmail.com>
2022-03-01 19:21:56 +0530

    Time: 11 ms (32.10%), Space: 7.9 MB (75.07%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,20 +1,9 @@
 class Solution {
 public:
-    vector<int> decToBinary(int n) {
-        vector<int> binaryNum(32);
-        int i = 0;
-        while(n > 0) {
-            binaryNum[i] = n % 2;
-            n = n / 2;
-            i++;
-        }
-        return binaryNum;
-    }
     vector<int> countBits(int n) {
-        vector<int> res;
-        for(int i=0; i<=n; ++i) {
-            vector<int> a = decToBinary(i);
-            res.push_back(count(a.begin(), a.end(), 1));
+        vector<int> res(n+1, 0);
+        for(int i=1; i<=n; ++i) {
+            res[i] = res[i & i-1] + 1;
         }
         return res;
     }
