552945ea798a066a9a97204d32e50113c8d8e200
Aditya Nath Jha <jhaaditya757@gmail.com>
2022-03-04 20:38:09 +0530

    Time: 4 ms (94.10%), Space: 10.8 MB (19.28%) - LeetHub

--- 1403-minimum-subsequence-in-non-increasing-order/1403-minimum-subsequence-in-non-increasing-order.cpp
+++ 1403-minimum-subsequence-in-non-increasing-order/1403-minimum-subsequence-in-non-increasing-order.cpp
@@ -3,12 +3,12 @@ public:
     vector<int> minSubsequence(vector<int>& a) {
         sort(a.begin(), a.end());
         int n = a.size(), sum = accumulate(a.begin(), a.end(), 0), curr = 0, i;
+        vector<int> res;
         for(i=n-1; i>=0; --i) {
+            res.push_back(a[i]);
             if(a[i] + curr > sum - a[i] - curr) break;
             curr += a[i];
         }
-        vector<int> res;
-        for(int j=n-1; j>=i; --j) res.push_back(a[j]);
         return res;
     }
 };
