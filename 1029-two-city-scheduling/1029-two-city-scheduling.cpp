1ae3b59fd3a20048ba247b210ba02a8efdab5f12
Aditya Nath Jha <jhaaditya757@gmail.com>
2022-03-25 21:03:40 +0530

    Time: 4 ms (78.75%), Space: 7.9 MB (47.20%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int twoCitySchedCost(vector<vector<int>>& a) {
-        int n = a.size()/2, res = 0, cnt = 0;
+        int n = a.size()/2, res = 0;
         sort(a.begin(), a.end(), [](vector<int>& x, vector<int>& y) {
             return x[0] - x[1] < y[0] - y[1];
         });
-        for(auto i: a) {
-            if(cnt < n) {
-                res += i[0];
-                cnt++;
-            } else
-                res += i[1];
-        }
+        for(int i=0; i<n; ++i) res += a[i][0];
+        for(int i=n; i<2*n; ++i) res += a[i][1];
         return res;
     }
 };
