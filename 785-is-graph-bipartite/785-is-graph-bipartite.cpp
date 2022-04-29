12d2ff37ae8373424acc9a79a1e70d43df23b28e
Aditya Nath Jha <jhaaditya757@gmail.com>
2022-04-29 15:49:35 +0530

    Time: 22 ms (89.32%), Space: 13.3 MB (99.18%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool visited[101];
+    int c[101];
+    bool dfs(int idx, int start, vector<vector<int>>& a) {
+        visited[idx] = true;
+        c[idx] = start;
+        for(auto i: a[idx]) {
+            if(visited[i]) {
+                if(c[idx] == c[i])
+                    return false;
+            } else {
+                if(!dfs(i, start^1, a))
+                    return false;
+            }
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& a) {
+        int n = a.size();
+        memset(visited, false, n);
+        memset(c, 0, n);
+        for(int i=0; i<n; ++i)
+            if(!visited[i] and !dfs(i,0,a))
+                return false;
+        return true;
+    }
+};
