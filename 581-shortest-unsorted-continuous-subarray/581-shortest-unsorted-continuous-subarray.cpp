0a6292d6e4c6995475600878ba1489f137d5af45
Aditya Nath Jha <jhaaditya757@gmail.com>
2022-05-03 21:39:51 +0530

    Time: 37 ms (68.60%), Space: 26.4 MB (89.65%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& a) {
-        int n = a.size(), l = n, r = 0;
-        stack<int> st;
-        for(int i=0; i<n; ++i) {
-            while(st.size() and a[st.top()] > a[i]) {
-                l = min(l, st.top());
-                st.pop();
-            }
-            st.push(i);
+        int n = a.size(), minm = INT_MAX, maxm = INT_MIN, l, r;
+        bool flag = false;
+        for(int i=1; i<n; ++i) {
+            if(a[i] < a[i-1]) flag = true;
+            if(flag) minm = min(minm, a[i]);
         }
-        st.empty();
-        for(int i=n-1; i>=0; --i) {
-            while(st.size() and a[st.top()] < a[i]) {
-                r = max(r, st.top());
-                st.pop();
-            }
-            st.push(i);
+        flag = false;
+        for(int i=n-2; i>=0; --i) {
+            if(a[i] > a[i+1]) flag = true;
+            if(flag) maxm = max(maxm, a[i]);
         }
-        if(r - l <= 0) return 0;
+        for(l=0; l<n; ++l)
+            if(minm < a[l]) break;
+        for(r=n-1; r>=0; --r)
+            if(maxm > a[r]) break;
+        if(r - l < 0) return 0;
         return r - l + 1;
     }
 };
