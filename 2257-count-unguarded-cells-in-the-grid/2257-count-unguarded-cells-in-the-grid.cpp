fee1792c494e951c866935831b2e10a936022b6c
Aditya Nath Jha <jhaaditya757@gmail.com>
2022-05-05 19:30:46 +0530

    Time: 1205 ms (14.31%), Space: 185.6 MB (24.10%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
@@ -23,11 +23,9 @@ public:
             }
         }
         int res = 0;
-        for(int i=0; i<m; ++i) {
-            for(int j=0; j<n; ++j) {
+        for(int i=0; i<m; ++i)
+            for(int j=0; j<n; ++j)
                 if(a[i][j] == 0) res++;
-            }
-        }
         return res;
     }
 };
