f0df0872ec27e5fcccd954465cde2e3708c4658e
Aditya Nath Jha <jhaaditya757@gmail.com>
2022-05-09 22:04:23 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (60.00%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -2,46 +2,28 @@ class Solution {
 public:
     vector<string> letterCombinations(string s) {
         int n = s.size();
-        if(n == 0) return {};
-        vector<string> a, res;
-        for(int i=0; i<n; ++i) {
-            if(s[i] == '2') a.push_back("abc");
-            if(s[i] == '3') a.push_back("def");
-            if(s[i] == '4') a.push_back("ghi");
-            if(s[i] == '5') a.push_back("jkl");
-            if(s[i] == '6') a.push_back("mno");
-            if(s[i] == '7') a.push_back("pqrs");
-            if(s[i] == '8') a.push_back("tuv");
-            if(s[i] == '9') a.push_back("wxyz");
-        }
+        if(!n) return {};
+        vector<string> num = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}, a, res;
+        for(auto i: s)
+            a.push_back(num[i - '0']);
         if(n == 1) {
-            for(auto i: a[0]) {
+            for(auto i: a[0])
                 res.push_back(string(1,i));
-            }
         } else if(n == 2) {
-            for(auto i: a[0]) {
-                for(auto j: a[1]) {
+            for(auto i: a[0])
+                for(auto j: a[1])
                     res.push_back(string(1,i) + j);
-                }
-            }
         } else if(n == 3) {
-            for(auto i: a[0]) {
-                for(auto j: a[1]) {
-                    for(auto k: a[2]) {
+            for(auto i: a[0])
+                for(auto j: a[1])
+                    for(auto k: a[2])
                         res.push_back(string(1,i) + j + k);
-                    }
-                }
-            }
         } else {
-            for(auto i: a[0]) {
-                for(auto j: a[1]) {
-                    for(auto k: a[2]) {
-                        for(auto l: a[3]) {
+            for(auto i: a[0])
+                for(auto j: a[1])
+                    for(auto k: a[2])
+                        for(auto l: a[3])
                             res.push_back(string(1,i) + j + k + l);
-                        }
-                    }
-                }
-            }
         }
         return res;
     }
