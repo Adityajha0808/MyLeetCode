bdb9000efa53f6d4057b606038c4fcf25b77b8a7
Aditya Nath Jha <jhaaditya757@gmail.com>
2022-05-18 19:38:15 +0530

    Time: 1409 ms (7.29%), Space: 166.7 MB (84.38%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -5,30 +5,25 @@ public:
         vi[u] = true;
         disc[u] = low[u] = time++;
         for(auto v : g[u]) {
-            if (!vi[v]) {
+            if(!vi[v]) {
                 parent[v] = u;
                 h(v, vi, disc, low, parent, g, res);
                 low[u] = min(low[u], low[v]);
-                if (low[v] > disc[u]) {
-                    res.push_back({u, v});
-                }
-
-            } else if (v != parent[u]) {
+                if(low[v] > disc[u]) res.push_back({u, v});
+            } else if(v != parent[u])
                 low[u] = min(low[u], disc[v]);
-            }
-
         }
     }
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& a) {
         vector<bool> vi(n, false);
         vector<int> disc(n, 0), low(n, 0), parent(n, -1);
         vector<vector<int>> g(n, vector<int>()), res;
-        for(auto & p : connections) {
+        for(auto & p : a) {
             g[p.front()].push_back(p.back());
             g[p.back()].push_back(p.front());
         }
-        for (int i = 0; i < n; ++i) {
-            if (vi[i]) continue;
+        for(int i = 0; i<n; ++i) {
+            if(vi[i]) continue;
             h(i, vi, disc, low, parent, g, res);
         }
         return res;
