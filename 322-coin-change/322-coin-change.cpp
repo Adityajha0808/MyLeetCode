e93cad09d40dc1d4137d1d3b00e8b8ec5382142a
Aditya Nath Jha <jhaaditya757@gmail.com>
2022-05-21 11:49:23 +0530

    Time: 347 ms (7.66%), Space: 19.4 MB (24.51%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int coinChange(vector<int>& a, int k) {
         int n = a.size();
-        sort(a.begin(), a.end());
+        // sort(a.begin(), a.end());
         vector<int> dp(10001,INT_MAX);
         dp[0] = 0;
         for(int i=0; i<=10000; ++i)
