0e072e10a80503e3efc9a442bf149cdcc5ad6ecb
Aditya Nath Jha <jhaaditya757@gmail.com>
2022-05-21 11:46:34 +0530

    Time: 345 ms (7.70%), Space: 19.5 MB (24.45%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    void func(vector<int>& a, int k, vector<int>& dp) {
+    int coinChange(vector<int>& a, int k) {
         int n = a.size();
+        sort(a.begin(), a.end());
+        vector<int> dp(10001,INT_MAX);
         dp[0] = 0;
         for(int i=0; i<=10000; ++i)
             for(int j=0; j<n; ++j)
                 if(a[j] <= i and dp[i - a[j]] != INT_MAX)
                     dp[i] = min(dp[i], dp[i - a[j]] + 1);
-    }
-    int coinChange(vector<int>& a, int k) {
-        sort(a.begin(), a.end());
-        vector<int> dp(10001,INT_MAX);
-        func(a,k,dp);
         return (dp[k] == INT_MAX ? -1 : dp[k]);
     }
 };
