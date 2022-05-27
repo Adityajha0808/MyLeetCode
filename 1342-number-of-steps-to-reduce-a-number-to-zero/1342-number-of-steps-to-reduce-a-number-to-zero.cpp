62a270351d3f61d4fb74d8f0dbe755908272f993
Aditya Nath Jha <jhaaditya757@gmail.com>
2022-05-27 15:31:52 +0530

    Time: 0 ms (100.00%), Space: 6 MB (26.66%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int numberOfSteps(int n) {
-        int steps = 0;
-        while(n) {
-            n = (n&1 ? n-1 : n/2);
-            steps++;
-        }
-        return steps;
+        if(n) return __builtin_popcount(n) + log2(n);
+        return 0;
     }
 };
