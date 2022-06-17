28f29f60ce44719b8a91117062063606ef01d0fc
Aditya Nath Jha <jhaaditya757@gmail.com>
2022-06-17 10:50:44 +0530

    Time: 44 ms (5.68%), Space: 23.9 MB (14.65%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int cams;
+    set<TreeNode*> st;
+public:
+    void func(TreeNode* root, TreeNode* p) {
+        if(root) {
+            func(root->left, root);
+            func(root->right, root);
+            if(p == NULL and st.find(root) == st.end() or st.find(root->left) == st.end() or st.find(root->right) == st.end()) {
+                cams++;
+                st.insert(root);
+                st.insert(p);
+                st.insert(root->left);
+                st.insert(root->right);
+            }
+        }
+    }
+    int minCameraCover(TreeNode* root) {
+        cams = 0;
+        st.insert(NULL);
+        func(root, NULL);
+        return cams;
+    }
+};
