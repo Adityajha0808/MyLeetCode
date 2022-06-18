06f93b6e1f5efe2fe36a6f5478745e7bb428ee2f
Aditya Nath Jha <jhaaditya757@gmail.com>
2022-06-18 12:11:49 +0530

    Time: 670 ms (57.84%), Space: 129.1 MB (75.75%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -6,7 +6,7 @@ public:
         int n = w.size();
         for(int i=n-1; ~i; --i) {
             int n2 = w[i].size();
-            if(mp2.count(w[i]) == 0) { 
+            if(!mp2.count(w[i])) { 
                 mp2[w[i]]++;
                 string pre = "";
                 for(int j=0; j < n2; ++j) {
@@ -14,9 +14,9 @@ public:
                     string suf = "";
                     for(int k = n2-1; ~k; --k) {
                         suf = w[i][k] + suf;
-                        if(mp.count(pre) == 0)
+                        if(!mp.count(pre))
                             mp[pre][suf] = i;
-                        else if(mp[pre].count(suf) == 0)
+                        else if(!mp[pre].count(suf))
                             mp[pre][suf] = i;
                     }
                 }
@@ -25,7 +25,7 @@ public:
     }
     
     int f(string pre, string suf) {
-        if(mp.count(pre) and mp[pre].count(suf) > 0)
+        if(mp.count(pre) and mp[pre].count(suf))
             return mp[pre][suf];
         return -1;
     }
