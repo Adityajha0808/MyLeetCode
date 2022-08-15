37e6a11dc0245793ce22bcf803a0234da2bcc1d3
Aditya Nath Jha <jhaaditya757@gmail.com>
2022-08-15 16:36:57 +0530

    Time: 456 ms (30.40%), Space: 106.9 MB (56.69%) - LeetHub

---
+++ 2333-minimum-sum-of-squared-difference/2333-minimum-sum-of-squared-difference.cpp
@@ -0,0 +1,48 @@
+using ll = long long;
+class Solution {
+public:
+    ll minSumSquareDiff(vector<int>& a1, vector<int>& a2, int k1, int k2) {
+        int n = a1.size(), k = k1 + k2;
+        unordered_map<int, int> mp;
+        for(int i=0; i<n; ++i) {
+            int tmp = abs(a1[i] - a2[i]);
+            if(tmp == 0) continue; 
+            mp[tmp]++;
+        }
+        priority_queue<pair<int, int>> pq;
+        for(auto i: mp) {
+            pq.push({i.first, i.second});
+        }
+        while(pq.size() and k > 0) {
+            auto tmp = pq.top();
+            pq.pop();
+            if(pq.size()) {
+                int minus = min(k, tmp.second);
+                k -= minus;
+                tmp.second -= minus;
+                if(tmp.second) pq.push(tmp);
+                if(pq.top().first == tmp.first - 1) {
+                    auto tmp2 = pq.top();
+                    pq.pop();
+                    tmp2.second += minus;
+                    pq.push(tmp2);
+                } else if(tmp.first - 1 > 0) {
+                    pq.push({tmp.first - 1, minus});
+                }
+            } else {
+                int minus = min(k, tmp.second);
+                k -= minus;
+                tmp.second -= minus;
+                if(tmp.first - 1 > 0)
+                    pq.push({tmp.first - 1, minus});
+                if(tmp.second) pq.push(tmp);
+            }
+        }
+        ll res = 0;
+        while(pq.size()) {
+            res += ((ll)pq.top().first * pq.top().first * pq.top().second);
+            pq.pop();
+        }
+        return res;
+    }
+};
