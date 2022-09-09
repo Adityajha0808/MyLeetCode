c25ee559d5e8d462c775c7a7c514fb528ed2b6cb
Aditya Nath Jha <jhaaditya757@gmail.com>
2022-09-09 20:07:30 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (92.86%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
-    bool funcCheck(int n, int x) {
-        vector<int> a, v;
-        while(n > 0) {
-            a.push_back(n%x);
-            n /= x;
-        }
-        v = a;
-        reverse(v.begin(), v.end());
-        return (a == v);
-    }
     bool isStrictlyPalindromic(int n) {
-        for(int i=2; i<n-1; ++i) {
-            if(!funcCheck(n, i)) return false;
-        }
-        return true;
+        return false;
     }
 };
