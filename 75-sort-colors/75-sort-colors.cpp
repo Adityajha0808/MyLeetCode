aa02ce0b3d881b8773a53352e7ec415ab20770a7
Aditya Nath Jha <jhaaditya757@gmail.com>
2022-09-29 19:39:55 +0530

    Time: 5 ms (30.45%), Space: 8.3 MB (69.89%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& a) {
-        int n = a.size(), zeroes = 0, ones = 0, twos = 0, j = 0;
-        for(int i=0; i<n; ++i) {
-            if(a[i] == 0) zeroes++;
-            if(a[i] == 1) ones++;
-            if(a[i] == 2) twos++;
-        }
-        while(zeroes--) {
-            a[j] = 0;
-            j++;
-        }
-        while(ones--) {
-            a[j] = 1;
-            j++;
-        }
-        while(twos--) {
-            a[j] = 2;
-            j++;
+        int n = a.size(), l = 0, m = 0, r = n-1;
+        while(m <= r) {
+            if(a[m] == 0) {
+                swap(a[m], a[l]);
+                l++;
+                m++;
+            } else if(a[m] == 1) {
+                m++;
+            } else {
+                swap(a[m], a[r]);
+                r--;
+            }
         }
     }
 };
