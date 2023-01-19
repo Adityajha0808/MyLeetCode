95664b0acf056800bd7ec28f6cdba27681d472e8
Aditya Nath Jha <jhaaditya757@gmail.com>
2023-01-19 23:05:07 +0530

    Added solution - LeetHub

--- Carpet into Box - GFG/carpet-into-box.cpp
+++ Carpet into Box - GFG/carpet-into-box.cpp
@@ -10,11 +10,8 @@ using namespace std;
 
 class Solution{
     public:
-    using ll = long long;
     int carpetBox(int A, int B, int C, int D) {
-        int a = A, b = B, c = C, d = D;
-        if((ll)a * b <= (ll)c * d) return 0;
-        int res1 = 0, res2 = 0;
+        int a = A, b = B, c = C, d = D, res1 = 0, res2 = 0;
         while(a > c) {
             a /= 2;
             res1++;
