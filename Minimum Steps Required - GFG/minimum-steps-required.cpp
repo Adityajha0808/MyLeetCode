0095fa88df337255c97a0a9b60d1ab0ae8e41709
Aditya Nath Jha <jhaaditya757@gmail.com>
2023-04-04 20:56:12 +0530

    Added solution - LeetHub

--- Minimum Steps Required - GFG/minimum-steps-required.cpp
+++ Minimum Steps Required - GFG/minimum-steps-required.cpp
@@ -9,7 +9,15 @@ using namespace std;
 // User function Template for C++
 
 int minSteps(string s) {
-    
+    int n = s.size(), a = 0, b = 0;
+    for(int i=1; i<n; ++i) {
+        if(s[i] == 'a' and s[i-1] == 'a') continue;
+        if(s[i-1] == 'a' and s[i] == 'b') a++;
+        if(s[i] == 'b' and s[i-1] == 'b') continue;
+        if(s[i-1] == 'b' and s[i] == 'a') b++;
+    }
+    s.back() == 'a' ? a++ : b++;
+    return min(a + 1, b + 1);
 }
 
 //{ Driver Code Starts.
